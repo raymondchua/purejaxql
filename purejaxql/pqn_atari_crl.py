b0481527bbf30d7277dc8e62279399aba4c8199b
Raymond Chua <ray.r.chua@gmail.com>
2025-04-10 15:32:17 -0400

    Add debug print statements for env_step and update_steps in pqn_atari_crl.py

--- purejaxql/pqn_atari_crl.py
+++ purejaxql/pqn_atari_crl.py
@@ -439,6 +439,11 @@ def make_train(config):
                             metrics[k] = v.item()
 
                         # print(f"{k}: {v}")
+                        if k == "env_step":
+                            print(f"{k}: {v}")
+
+                        if k == "update_steps":
+                            print(f"{k}: {v}")
 
                     wandb.log(metrics, step=metrics["update_steps"])
 
