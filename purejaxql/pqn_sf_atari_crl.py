eea9fab4b1e4aaeab745472a4318a3189abad03a
Raymond Chua <ray.r.chua@gmail.com>
2025-04-12 15:24:38 -0400

    Fix variable name for multi-training state in SF-PQN agent

--- purejaxql/pqn_sf_atari_crl.py
+++ purejaxql/pqn_sf_atari_crl.py
@@ -608,7 +608,7 @@ def single_run(config):
             else:
                 # outs = jax.jit(make_train(config))(rng, exposure)
                 outs = jax.jit(
-                    lambda rng: make_train(config)(rng, cycle, train_state, network, task_id)
+                    lambda rng: make_train(config)(rng, cycle, multi_train_state, network, task_id)
                 )(rng)
             print(f"Took {time.time()-start_time} seconds to complete.")
 
