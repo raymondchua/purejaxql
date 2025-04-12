470014f4f03abd414fbe2078f32c5483583dde94
Raymond Chua <ray.r.chua@gmail.com>
2025-04-11 21:49:31 -0400

    Fix duplicate environment name in task configuration for 5 tasks in pqn_atari_crl.py

--- purejaxql/pqn_atari_crl.py
+++ purejaxql/pqn_atari_crl.py
@@ -534,7 +534,7 @@ def single_run(config):
     if config["alg"]["NUM_TASKS"] == 3:
         env_names = "Pong-v5, Breakout-v5, SpaceInvaders-v5"
     elif config["alg"]["NUM_TASKS"] == 5:
-        env_names = "Alien-v5, Atlantis2-v5, Boxing-v5, Breakout-v5, Centipede-v5"
+        env_names = "Alien-v5, Boxing-v5, Boxing-v5, Breakout-v5, Centipede-v5"
 
     elif config["alg"]["NUM_TASKS"] < 3:
         raise NotImplementedError("Less than 3 games not supported yet.")
