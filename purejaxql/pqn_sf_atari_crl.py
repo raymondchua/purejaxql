Fix task normalization dimension in basis features calculation for accuracy

purejaxql/pqn_sf_atari_crl.py
@@ -94,7 +94,7 @@ class SFNetwork(nn.Module):
 
         task = jax.lax.stop_gradient(task)
         task_normalized = task / jnp.linalg.norm(task, ord=2, axis=-1, keepdims=True)
-        task_normalized = jnp.expand_dims(task_normalized, 0)
+        task_normalized = jnp.expand_dims(task_normalized, 1)
         task_normalized = jnp.tile(task_normalized, (x.shape[0], 1))
         # task = convert_variable_into_batch(task, batch_size=x.shape[0])
 