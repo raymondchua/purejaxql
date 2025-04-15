Fix reward loss computation to use a subset of task parameters for updates

purejaxql/pqn_sf_atari_crl.py
@@ -378,7 +378,7 @@ def make_train(config):
                     "batch_stats": train_state.network_state.batch_stats,
                 },
                 transitions.next_obs[-1],
-                train_state.task_state.params["w"],
+                train_state.task_state.params["w"][: -config["TEST_ENVS"], :],
                 train=False,
             )
             last_q = jnp.max(last_q, axis=-1)