232f26a2f29332b524a3c092acbccba1f4e6ec34
Raymond Chua <ray.r.chua@gmail.com>
2025-04-15 16:02:53 -0400

    Add logging statement for metrics in WANDB callback

--- purejaxql/pqn_craftax.py
+++ purejaxql/pqn_craftax.py
@@ -395,6 +395,7 @@ def make_train(config):
                     if (
                         metrics["update_steps"] % config.get("WANDB_LOG_INTERVAL", 128) == 0
                     ):
+                        print("Logging metrics to wandb")
                         if config.get("WANDB_LOG_ALL_SEEDS", False):
                             metrics.update(
                                 {
