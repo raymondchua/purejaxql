9aac46fbf563d6066cb811c11b0c5a17153df1ee
Raymond Chua <ray.r.chua@gmail.com>
2025-04-15 20:58:55 -0400

    Remove unnecessary logging of metrics in pqn_craftax.py

--- purejaxql/pqn_craftax.py
+++ purejaxql/pqn_craftax.py
@@ -396,11 +396,6 @@ def make_train(config):
                     if (
                         metrics["update_steps"] % config.get("WANDB_LOG_INTERVAL", 128) == 0
                     ):
-                        print("Logging metrics to wandb")
-
-                        for k,v in metrics.items():
-                            print(f"{k}: {v}")
-
                         if config.get("WANDB_LOG_ALL_SEEDS", False):
                             metrics.update(
                                 {
