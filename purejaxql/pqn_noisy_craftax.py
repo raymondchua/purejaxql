96e2347e0ab83aee9da0b556b5c27294b0dc542f
Raymond Chua <ray.r.chua@gmail.com>
2025-04-18 13:22:44 -0400

    Enhance agent initialization by adding noise_rng for improved randomness

--- purejaxql/pqn_noisy_craftax.py
+++ purejaxql/pqn_noisy_craftax.py
@@ -139,7 +139,8 @@ def make_train(config):
 
         def create_agent(rng):
             init_x = jnp.zeros((1, *env.observation_space(env_params).shape))
-            network_variables = network.init(rng, init_x, train=False)
+            _, noise_rng = jax.random.split(rng)
+            network_variables = network.init(rng, init_x, noise_rng=noise_rng, train=False)
             tx = optax.chain(
                 optax.clip_by_global_norm(config["MAX_GRAD_NORM"]),
                 optax.radam(learning_rate=lr),
