f18c05ac276105228b90a5abdef0344ea3bdd813
Raymond Chua <ray.r.chua@gmail.com>
2025-04-21 17:14:55 -0400

    Set fixed name for convolution layer in pqn_sc_task_atari_crl.py for consistency

--- purejaxql/pqn_sc_task_atari_crl.py
+++ purejaxql/pqn_sc_task_atari_crl.py
@@ -50,7 +50,7 @@ class CNN(nn.Module):
             padding="VALID",
             kernel_init=nn.initializers.he_normal(),
             num_tasks=self.num_tasks,
-            name=name,
+            name="conv_2",
         )(x, task_id)
         x = normalize(x)
         x = nn.relu(x)
