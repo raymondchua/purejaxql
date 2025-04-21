6d7ef094868ef15329d0c39d6f46e4cdfd7ada30
Raymond Chua <ray.r.chua@gmail.com>
2025-04-21 16:00:26 -0400

    Add parameter name logging to metrics in Atari CRL training

--- purejaxql/pqn_sc_task_atari_crl.py
+++ purejaxql/pqn_sc_task_atari_crl.py
@@ -142,7 +142,7 @@ def create_agent(rng, config, max_num_actions, observation_space_shape):
     )
 
     init_x = jnp.zeros((1, *observation_space_shape))
-    network_variables = network.init(rng, init_x, train=False)
+    network_variables = network.init(rng, init_x, task_id=0, train=False)
 
     tx = optax.chain(
         optax.clip_by_global_norm(config["MAX_GRAD_NORM"]),
@@ -300,7 +300,7 @@ def make_train(config):
                     },
                     last_obs,
                     train=False,
-                    task_id=task_id,
+                    task_id=unique_task_id,
                 )
 
                 # different eps for each env
@@ -360,7 +360,7 @@ def make_train(config):
                 },
                 transitions.next_obs[-1],
                 train=False,
-                task_id=task_id,
+                task_id=unique_task_id,
             )
             last_q = jnp.max(last_q, axis=-1)
 
