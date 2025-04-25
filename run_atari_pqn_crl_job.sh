58642843b6442e4d9a243a818affb7329e83ad44
Raymond Chua <ray.r.chua@gmail.com>
2025-04-25 10:48:28 -0400

    Update SF_DIM parameter in experiment execution command for enhanced performance

--- run_atari_pqn_crl_job.sh
+++ run_atari_pqn_crl_job.sh
@@ -43,4 +43,4 @@ source /home/chuaraym/pqn_atari_env311/bin/activate
 cd /home/chuaraym/purejaxql/
 
 # Run the experiment
-python purejaxql/pqn_sf_task_atari_crl.py +alg=pqn_sf_task_atari_crl SEED=${SEED} SAVE_PATH=${OUT_DIR} alg.SF_DIM=8
+python purejaxql/pqn_sf_task_atari_crl.py +alg=pqn_sf_task_atari_crl SEED=${SEED} SAVE_PATH=${OUT_DIR} alg.SF_DIM=32
