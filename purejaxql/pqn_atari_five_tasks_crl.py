6baa2b5ab60e6ad08498c4d2d90c80b057030829
Raymond Chua <ray.r.chua@gmail.com>
2025-04-26 14:24:53 -0400

    Increase total timesteps for primary and secondary tasks in PQN Atari configuration

--- purejaxql/pqn_atari_five_tasks_crl.py
+++ purejaxql/pqn_atari_five_tasks_crl.py
@@ -562,11 +562,11 @@ def single_run(config):
 
             # first task is the primary task, which has longer training time
             if unique_task_id == 0:
-                config["TOTAL_TIMESTEPS"] = 100000
-                config["TOTAL_TIMESTEPS_DECAY"] = 100000
+                config["TOTAL_TIMESTEPS"] = 1000000
+                config["TOTAL_TIMESTEPS_DECAY"] = 1000000
             else:
-                config["TOTAL_TIMESTEPS"] = 90000
-                config["TOTAL_TIMESTEPS_DECAY"] = 90000
+                config["TOTAL_TIMESTEPS"] = 900000
+                config["TOTAL_TIMESTEPS_DECAY"] = 900000
 
             config["ENV_NAME"] = env_name
             if config["NUM_SEEDS"] > 1:
