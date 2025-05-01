a4ada485de8a63fc4f0e404b2626c7d1c39f1dd5
Raymond Chua <ray.r.chua@gmail.com>
2025-05-01 14:25:11 -0400

    Rename cosine_similarity.py to similarity.py and add RBF similarity function

--- purejaxql/utils/similarity.py
+++ purejaxql/utils/similarity.py
@@ -5,3 +5,8 @@ def cosine_similarity(a, b, eps=1e-8):
     numerator = jnp.sum(a * b, axis=-1)  # (batch_size, num_beakers-1)
     denominator = jnp.linalg.norm(a, axis=-1) * jnp.linalg.norm(b, axis=-1) + eps  # (batch_size, num_beakers-1)
     return numerator / denominator
+
+
+def rbf_similarity(a, b, sigma=1.0):
+    squared_distance = jnp.sum((a - b) ** 2, axis=-1)  # (batch_size, num_beakers-1)
+    return jnp.exp(-squared_distance / (2 * sigma ** 2))  # (batch_size, num_beakers-1)
