e0ff2bc3b40db0e27d70efb9dcee247dcde8de11
Raymond Chua <ray.r.chua@gmail.com>
2025-05-01 16:03:10 -0400

    Fix input to query layer in attention mechanism to use concatenated basis features

--- purejaxql/pqn_sf_sc_attention_atari_crl.py
+++ purejaxql/pqn_sf_sc_attention_atari_crl.py
@@ -180,7 +180,7 @@ class SFAttentionNetwork(nn.Module):
         basis_features_sf_first_task = jnp.concatenate([basis_features_first, sf_first_reshaped, task_normalized[:, :1, :]], axis=-1)
         print("basis_features_sf_first_task shape:", basis_features_sf_first_task.shape)
 
-        query = nn.Dense(features=self.sf_dim * 3 * self.proj_factor, name="query", use_bias=False)(task_basis_feat_sf)
+        query = nn.Dense(features=self.sf_dim * 3 * self.proj_factor, name="query", use_bias=False)(basis_features_sf_first_task)
         print("query shape:", query.shape)
 
         # Different dense layers for each beaker to compute keys and values
