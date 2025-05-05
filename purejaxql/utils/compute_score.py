Fix done_repeat calculation to use the correct achievements dimension

purejaxql/utils/compute_score.py
@@ -5,7 +5,8 @@ Source: Craftax-Foraging project
 """
 
 def compute_score(state, done):
-    done_repeat = jnp.repeat(done, state.env_state.achievements.shape[0], axis=0)
+    # repeat done for each achievement
+    done_repeat = jnp.repeat(done, state.env_state.achievements.shape[1], axis=0)
     print("done_repeat shape: ", done_repeat.shape)
     achievements = state.env_state.achievements * done * 100.0
     info = {}