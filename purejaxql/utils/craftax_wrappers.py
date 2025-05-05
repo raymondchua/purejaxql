1199c1c501473bcc8869936d96d4e41eb8f03631
Raymond Chua <ray.r.chua@gmail.com>
2025-05-05 16:15:11 -0400

    Fix step function to use correct variable name for observation state

--- purejaxql/utils/craftax_wrappers.py
+++ purejaxql/utils/craftax_wrappers.py
@@ -214,7 +214,7 @@ class AddScoreEnvWrapper(GymnaxWrapper):
 
     @partial(jax.jit, static_argnums=(0, 4))
     def step(self,  key: chex.PRNGKey, state, action, params=None):
-        obs, state_st, reward, done, info = self._env.step(
+        obs_st, state_st, reward, done, info = self._env.step(
             key, state, action, params
         )
 
