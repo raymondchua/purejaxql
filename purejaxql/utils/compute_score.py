45d03fab190c84a4995d0c1d37a54eb54a760b2e
Raymond Chua <ray.r.chua@gmail.com>
2025-05-05 15:55:19 -0400

    Add debug print statement for state in compute_score function

--- purejaxql/utils/compute_score.py
+++ purejaxql/utils/compute_score.py
@@ -5,6 +5,7 @@ Source: Craftax-Foraging project
 """
 
 def compute_score(state, done):
+    print("state: ", state)
     achievements = state.achievements * done * 100.0
     info = {}
     # Geometric mean with an offset of 1%
