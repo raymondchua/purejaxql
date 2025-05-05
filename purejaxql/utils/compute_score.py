Fix done_repeat calculation to reference env_state achievements

purejaxql/utils/compute_score.py
@@ -5,7 +5,7 @@ Source: Craftax-Foraging project
 """
 
 def compute_score(state, done):
-    done_repeat = jnp.repeat(done, state.achievements.shape[0], axis=0)
+    done_repeat = jnp.repeat(done, state.env_state.achievements.shape[0], axis=0)
     print("done_repeat shape: ", done_repeat.shape)
     achievements = state.env_state.achievements * done * 100.0
     info = {}