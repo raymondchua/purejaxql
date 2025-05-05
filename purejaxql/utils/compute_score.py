544409b6f3b1336f31056ab2b9d5a9912a6e5b92
Raymond Chua <ray.r.chua@gmail.com>
2025-05-05 16:03:23 -0400

    Refactor compute_score function to use env_state achievements directly

--- purejaxql/utils/compute_score.py
+++ purejaxql/utils/compute_score.py
@@ -5,9 +5,7 @@ Source: Craftax-Foraging project
 """
 
 def compute_score(state, done):
-    print("achievements: ", state.env_state.achievements)
-    print("done: ", done)
-    achievements = state.achievements * done * 100.0
+    achievements = state.env_state.achievements * done * 100.0
     info = {}
     # Geometric mean with an offset of 1%
     info["score"] = jnp.exp(jnp.mean(jnp.log(1 + achievements))) - 1.0
