bc3d096e5f620f31164bd5a8a1fe7a6034abfda4
Raymond Chua <ray.r.chua@gmail.com>
2025-05-07 18:07:05 -0400

    Update script to run pqn_rnn_craftax instead of pqn_sf_sc_attention_craftax

--- run_craftax_job.sh
+++ run_craftax_job.sh
@@ -43,4 +43,4 @@ source /home/chuaraym/pqn_env311/bin/activate
 cd /home/chuaraym/purejaxql/
 
 # Run the experiment
-python purejaxql/pqn_sf_sc_attention_craftax.py +alg=pqn_sf_sc_attention_craftax SEED=${SEED} SAVE_PATH=${OUT_DIR} PROJECT="Craftax"
+python purejaxql/pqn_rnn_craftax.py +alg=pqn_rnn_craftax SEED=${SEED} SAVE_PATH=${OUT_DIR} PROJECT="Craftax"
