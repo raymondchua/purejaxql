47eb327ce565515c0837de2c8e3ff70a8760f4b4
Raymond Chua <ray.r.chua@gmail.com>
2025-05-07 16:54:45 -0400

    Enable logging of metrics in pqn_rnn_craftax.py

--- purejaxql/pqn_rnn_craftax.py
+++ purejaxql/pqn_rnn_craftax.py
@@ -499,8 +499,8 @@ def make_train(config):
                     # metrics.update({k: v for k, v in to_log.items()})
                     # batch_log(metrics["update_steps"], metrics, config)
                     #
-                    # for k, v in metrics.items():
-                    #     print(f"{k}: {v}")
+                    for k, v in metrics.items():
+                        print(f"{k}: {v}")
 
                 jax.debug.callback(callback, metrics, original_rng)
 
